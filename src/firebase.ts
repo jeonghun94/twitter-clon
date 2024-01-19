60ec63e6262c3bd012cefbf570fc99a658df97d0
조정훈 <jeongh1021@naver.com>
2024-01-19 14:42:04 +0900

    강의 기능 구현 완료

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,8 @@
 // jjhcmcni@gmail.com
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,4 +18,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
